804f2829645321163c78dd5e9f6c32145dcc6361
Ronald <ronald@contentdokter.nl>
2015-03-26 15:11:51 +0100

    initial

--- src/DoctorBeat/EloquentRepository/EloquentRepository.php
+++ src/DoctorBeat/EloquentRepository/EloquentRepository.php
@@ -15,19 +15,39 @@ class EloquentRepository implements Repository {
         $this->modelClassname = $modelClassname;
     }
 
-
+    /**
+     * Get all of the models from the database.
+	 * @param  array  $columns
+	 * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function all() {
         return call_user_func_array("{$this->modelClassname}::all", array());        
     }
 
+	/**
+	 * Delete the model from the database.
+	 *
+	 * @return bool|null
+	 * @throws \Exception
+	 */
     public function delete($entity) {
         return $entity->delete();        
     }
     
+	/**
+	 * Save the model and all of its relationships.
+	 *
+	 * @return bool
+	 */
     public function push($entity) {
         return $entity->push();        
     }
     
+	/**
+	 * Update the model's update timestamp.
+	 *
+	 * @return bool
+	 */
     public function touch($entity) {
         if (method_exists($entity, 'touch')) {
             return $entity->touch();
@@ -38,10 +58,23 @@ class EloquentRepository implements Repository {
         }
     }
     
+	/**
+	 * Find a model by its primary key.
+	 *
+	 * @param  mixed  $id
+	 * @param  array  $columns
+	 * @return \Illuminate\Support\Collection|static|null
+	 */
     public function find($id, $columns = array('*')) {
         return call_user_func_array("{$this->modelClassname}::find", array($id, $columns));
 	}
 
+	/**
+	 * Save the model to the database.
+	 *
+	 * @param  array  $options
+	 * @return bool
+	 */
     public function save($entity) {
         return $entity->save();
     }
